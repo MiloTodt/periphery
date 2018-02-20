569065aaa2a5f35a7dad04e2190c19c617b37cc5
Milo Todt <Milo.Todt@gmail.com>
2018-02-20 15:08:04 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-var request = require('request'); //For IP lookup of guests. 
-
-request('https://iptoearth.expeditedaddons.com/?api_key=4G13RQ86DIZ1072ST0WFL9P5KCJA64HOMXUE3259NV78BY&ip=68.10.149.45', function (error, response, body) {
-  console.log('Status:', response.statusCode);
-  console.log('Headers:', JSON.stringify(response.headers));
-  console.log('Response:', body);
-});
 
 express()
 
