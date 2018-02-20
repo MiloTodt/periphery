d03d8462f61746e21913f534fbd160a53e422ae2
milotodt <milo@milotodt.com>
2018-02-20 00:36:30 -0800

    node

--- index.js
+++ index.js
@@ -1,65 +1,10 @@
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-var url = require('url');
-var qs = require('querystring');
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-var server = http.createServer();
-server.on('request', function (req, res) {
-    console.log('request:', req.url);
-    var urlObj = url.parse(req.url, true);
-    console.log(urlObj.query.lname);
-    if (req.method === 'GET' && req.url.match(/^\/.+\.html$/)) {
-        var filepath = (__dirname, req.url);
-        fs.readFile(filepath, function (err, contents) {
-            if (err) { // handle error 
-
-            } else {
-                res.writeHead(200, {
-                    "Content-Type": "text/html"
-                });
-                res.write(contents);
-                res.end();
-            }
-        });
-    } else if (req.method === 'GET' && req.url.match(/^\/.+\.jpg$/)) {
-        var imgpath = path.join(__dirname, req.url);
-        var imgstream = fs.createReadStream(imgpath, {
-            highWaterMark: 1024
-        });
-        res.writeHead(200, {
-            "Content-Type": "image/jpeg"
-        });
-        imgstream.pipe(res);
-    } else if (req.url === '/') {
-        html = fs.readFileSync('./index.html');
-        res.setHeader('Content-Type', 'text/html');
-        res.writeHead(200, {
-            'Content-Type': 'text/html'
-        });
-        res.end(html);
-
-    } else if (req.url === '/style.css') {
-        html = fs.readFileSync('./style.css');
-        res.setHeader('Content-Type', 'text/css');
-        res.writeHead(200, {
-            'Content-Type': 'text/css'
-        });
-        res.end(html);
-    } else if (req.url === '/script.js') {
-        html = fs.readFileSync('./script.js');
-        res.setHeader('Content-Type', 'text/js');
-        res.writeHead(200, {
-            'Content-Type': 'text/js'
-        });
-        res.end(html);
-    } else {
-        var html404 = "404 error!";
-        res.setHeader('Content-Type', 'text/html');
-        res.writeHead(200, {
-            'Content-Type': 'text/html'
-        });
-        res.end(html);
-    }
-});
-server.listen(8080);
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
