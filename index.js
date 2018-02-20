1ecdf5401f0fb6f85d1e3f66279753073d3e4b67
milotodt <milo@milotodt.com>
2018-02-20 00:38:01 -0800

    credentials setup

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
+
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
