b86b39a460c758cf1a544b1fb7554b97c6c27e2d
milotodt <milo@milotodt.com>
2018-03-06 01:44:37 -0800

    Guide buttons now work, awaiting guide to be written.

--- public/js/init.js
+++ public/js/init.js
@@ -6,3 +6,14 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+function guideWindows() {
+  document.getElementById("guide").innerHTML = "<br/>Guide for windows <li> Step 1</li><li> Step 2</li><li> Step 3</li>";
+} 
+function guideOSx() {
+  document.getElementById("guide").innerHTML = "<br/>Guide for OSx<li> Step 1</li><li> Step 2</li><li> Step 3</li>";
+} 
+function guideLinux() {
+  document.getElementById("guide").innerHTML = "<br/>Guide for Linux<li> Step 1</li><li> Step 2</li><li> Step 3</li>";
+}
+Z
